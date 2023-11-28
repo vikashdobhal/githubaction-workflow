3e28cfb9c1c3ee78342c1b532d57f243e9d847f9
Dobhal <BAM20516@superioruniform.com>
2023-11-28 17:59:25 +0530

    update files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ci-cd-testing/", // Add only this section
+  base: "/githubaction-workflow/", // Add only this section
 })
