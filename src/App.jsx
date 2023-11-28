a37d62b13ca0b71ae916eb6c07f90697c7150287
Dobhal <BAM20516@superioruniform.com>
2023-11-28 18:12:35 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <h1>Welcome To my First CI/CD workflow with github action!!</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
